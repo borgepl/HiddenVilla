f6fefa931363aaf6e33128e2cd178c1bd3bcd379
borgepl <pdeborger@gmail.com>
2023-08-09 14:28:34 +0200

    HotelRoomService to retrieve from API with HttpClient

---
+++ HiddenVilla_Client/Services/Contracts/IHotelRoomService.cs
@@ -0,0 +1,10 @@
+﻿using Models.Dto;
+
+namespace HiddenVilla_Client.Services.Contracts
+{
+    public interface IHotelRoomService
+    {
+        public Task<IEnumerable<HotelRoomDTO>> GetHotelRooms(string checkInDate, string checkOutDate);
+        public Task<HotelRoomDTO> GetHotelRoomDetails(int roomId, string checkInDate, string checkOutDate);
+    }
+}
---
+++ HiddenVilla_Client/Services/HotelRoomService.cs
@@ -0,0 +1,28 @@
+﻿using HiddenVilla_Client.Services.Contracts;
+using Models.Dto;
+using Newtonsoft.Json;
+
+namespace HiddenVilla_Client.Services
+{
+    public class HotelRoomService : IHotelRoomService
+    {
+        private readonly HttpClient _client;
+
+        public HotelRoomService(HttpClient client)
+        {
+            _client = client;
+        }
+        public Task<HotelRoomDTO> GetHotelRoomDetails(int roomId, string checkInDate, string checkOutDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<HotelRoomDTO>> GetHotelRooms(string checkInDate, string checkOutDate)
+        {
+            var response = await _client.GetAsync($"api/hotelroom?checkInDate={checkInDate}&checkOutDate={checkOutDate}");
+            var content = await response.Content.ReadAsStringAsync();
+            var rooms = JsonConvert.DeserializeObject<IEnumerable<HotelRoomDTO>>(content);
+            return rooms;
+        }
+    }
+}
