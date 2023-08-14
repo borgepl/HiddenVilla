28e5207fb80fcbc86536fea938bb10b092c9638e
borgepl <pdeborger@gmail.com>
2023-08-14 15:00:25 +0200

    Interface for AuthenticationService in client

---
+++ HiddenVilla_Client/Services/Contracts/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using Models.Dto.Login;
+using Models.Dto.Registration;
+
+namespace HiddenVilla_Client.Services.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResponseDTO> RegisterUser(UserRequestDTO userForRegisteration);
+
+        Task<AuthenticationResponseDTO> Login(AuthenticationDTO userFromAuthentication);
+
+        Task Logout();
+    }
+}
