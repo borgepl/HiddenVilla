51e3c1373128395e9c33eacc31e8e829e0cce751
borgepl <pdeborger@gmail.com>
2023-08-14 13:56:08 +0200

    Json Token Parser and AuthenticationStateProvider

---
+++ HiddenVilla_Client/Services/AuthStateProvider.cs
@@ -0,0 +1,34 @@
+﻿using Blazored.LocalStorage;
+using Common;
+using HiddenVilla_Client.Helper;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace HiddenVilla_Client.Services
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>(SD.Local_Token);
+            if (token == null)
+            {
+                return AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+        }
+    }
+}
