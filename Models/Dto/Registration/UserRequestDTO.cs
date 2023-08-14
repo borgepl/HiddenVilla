f3b2d54c46183ff523f968660a73a388d350807a
borgepl <pdeborger@gmail.com>
2023-08-14 15:58:35 +0200

    Login and Register UI

--- Models/Dto/Registration/UserRequestDTO.cs
+++ Models/Dto/Registration/UserRequestDTO.cs
@@ -10,20 +10,20 @@ namespace Models.Dto.Registration
     public class UserRequestDTO
     {
         [Required(ErrorMessage = "Name is required")]
-        public required string Name { get; set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
         [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email address")]
-        public required string Email { get; set; }
+        public string Email { get; set; }
 
         public string? PhoneNo { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
-        public required string Password { get; set; }
+        public string Password { get; set; }
         [Required(ErrorMessage = "Confirm password is required")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Password and confirm password is not matched")]
-        public required string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
