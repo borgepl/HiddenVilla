2dd574939c49a87e4fa6c63b6553fb630a5e3a74
borgepl <pdeborger@gmail.com>
2023-08-16 15:02:51 +0200

    Not Authorized scenario - RedirectToLogin

---
+++ HiddenVilla_Client/Pages/Authentication/RedirectToLogin.razor.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class RedirectToLogin
+    {
+        [Inject]
+        private NavigationManager navigationManager { get; set; }
+
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationState { get; set; }
+
+        bool notAuthorized { get; set; } = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await authenticationState;
+
+            if (authState?.User?.Identity is null || !authState.User.Identity.IsAuthenticated)
+            {
+                var returnUrl = navigationManager.ToBaseRelativePath(navigationManager.Uri);
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    navigationManager.NavigateTo("login", true);
+                }
+                else
+                {
+                    navigationManager.NavigateTo($"login?returnUrl={returnUrl}", true);
+                }
+            }
+            else
+            {
+                notAuthorized = true;
+            }
+
+
+        }
+    }
+}
